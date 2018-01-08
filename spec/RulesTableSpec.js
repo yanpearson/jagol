f47fb12764c0632235029a1e0ce142567bd02c51
yan.pearson <yan.pearson@gmail.com>
2018-01-07 23:16:17 -0500

    Minor optimizations
    Slightly better performances using typed arrays.

--- spec/RulesTableSpec.js
+++ spec/RulesTableSpec.js
@@ -3,6 +3,9 @@
 const R = require('ramda');
 const game = require('../lib/jagol');
 
+const DEAD_STATE = 0;
+const ALIVE_STATE = 255;
+
 describe("Rules table", function() {
 
 	it("does generate", function() {
@@ -11,36 +14,36 @@ describe("Rules table", function() {
 		// 0 0 0
 		// 0 0 0
 		// 0 0 0
-		expect(rulesTable[0]).toBe(false);
+		expect(rulesTable[0]).toBe(DEAD_STATE);
 
 		// 1 0 0
 		// 1 0 0
 		// 0 0 0
-		expect(rulesTable[3]).toBe(false);
+		expect(rulesTable[3]).toBe(DEAD_STATE);
 
 		// 1 0 0
 		// 1 0 0
 		// 1 0 0
-		expect(rulesTable[7]).toBe(true);
+		expect(rulesTable[7]).toBe(ALIVE_STATE);
 
 		// 1 0 0
 		// 1 1 0
 		// 0 0 0
-		expect(rulesTable[19]).toBe(true);
+		expect(rulesTable[19]).toBe(ALIVE_STATE);
 
 		// 0 1 0
 		// 0 1 0
 		// 0 1 0
-		expect(rulesTable[56]).toBe(true);
+		expect(rulesTable[56]).toBe(ALIVE_STATE);
 
 		// 0 0 1
 		// 0 1 1
 		// 1 0 0
-		expect(rulesTable[212]).toBe(true);
+		expect(rulesTable[212]).toBe(ALIVE_STATE);
 
 		// 1 1 1
 		// 1 1 1
 		// 1 1 1
-		expect(rulesTable[511]).toBe(false);
+		expect(rulesTable[511]).toBe(DEAD_STATE);
 	});
 });
