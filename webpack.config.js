711b62583686b252f77d76c5e303c26b1d8d647f
yan.pearson <yan.pearson@gmail.com>
2018-03-01 23:19:38 -0500

    Adding previous/next gen capability

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
 	entry: {
 		jagol: './lib/jagol/index.js',
 		patterns: './lib/patterns/index.js',
+		immutable: 'immutable'
 	},
 	output: {
 		filename: '[name].js',
