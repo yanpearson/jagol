e70346c40bb31ea49846498e21c2f1a56dfaddfd
yan.pearson <yan.pearson@gmail.com>
2018-03-18 13:45:52 -0400

    Doing minor refactors
    One step closer to the next feature

--- spec/HashTableSpec.js
+++ spec/HashTableSpec.js
@@ -3,47 +3,78 @@
 const R = require('ramda');
 const game = require('../lib/jagol');
 
-const DEAD_STATE = game.LifeState.dead;
-const ALIVE_STATE = game.LifeState.alive;
+const NeighborhoodState = game.NeighborhoodState;
+const CellState = game.CellState;
 
-describe("Hash table", function() {
+describe("When creating an hash table", function() {
 
-	it("does generate", function() {
+	it("contains valid data", function() {
 		const hashTable = game.createHashTable();
+		let hash = 0;
 
 		// 0 0 0
 		// 0 0 0
 		// 0 0 0
-		expect(hashTable[0]).toBe(DEAD_STATE);
+		expect(hashTable[hash]).toBe(CellState.dead);
 
 		// 1 0 0
 		// 1 0 0
 		// 0 0 0
-		expect(hashTable[3]).toBe(DEAD_STATE);
+		hash = NeighborhoodState.neighbor1Alive
+			 + NeighborhoodState.neighbor2Alive;
+
+		expect(hashTable[hash]).toBe(CellState.dead);
 
 		// 1 0 0
 		// 1 0 0
 		// 1 0 0
-		expect(hashTable[7]).toBe(ALIVE_STATE);
+		hash = NeighborhoodState.neighbor1Alive
+			 + NeighborhoodState.neighbor2Alive
+			 + NeighborhoodState.neighbor3Alive;
+
+		expect(hashTable[hash]).toBe(CellState.alive);
 
 		// 1 0 0
 		// 1 1 0
 		// 0 0 0
-		expect(hashTable[19]).toBe(ALIVE_STATE);
+		hash = NeighborhoodState.neighbor1Alive
+			 + NeighborhoodState.neighbor2Alive
+			 + CellState.alive;
+
+		expect(hashTable[hash]).toBe(CellState.alive);
 
 		// 0 1 0
 		// 0 1 0
 		// 0 1 0
-		expect(hashTable[56]).toBe(ALIVE_STATE);
+		hash = NeighborhoodState.neighbor4Alive
+			 + NeighborhoodState.neighbor5Alive
+			 + CellState.alive;
+
+		expect(hashTable[hash]).toBe(CellState.alive);
 
 		// 0 0 1
 		// 0 1 1
 		// 1 0 0
-		expect(hashTable[100]).toBe(ALIVE_STATE);
+		hash = NeighborhoodState.neighbor3Alive
+			 + NeighborhoodState.neighbor6Alive
+			 + NeighborhoodState.neighbor7Alive
+			 + CellState.alive;
+
+		expect(hashTable[hash]).toBe(CellState.alive);
 
 		// 1 1 1
 		// 1 1 1
 		// 1 1 1
-		expect(hashTable[255]).toBe(DEAD_STATE);
+		hash = NeighborhoodState.neighbor1Alive
+			 + NeighborhoodState.neighbor2Alive
+			 + NeighborhoodState.neighbor3Alive
+			 + NeighborhoodState.neighbor4Alive
+			 + NeighborhoodState.neighbor5Alive
+			 + NeighborhoodState.neighbor6Alive
+			 + NeighborhoodState.neighbor7Alive
+			 + NeighborhoodState.neighbor8Alive
+			 + CellState.alive;
+
+		expect(hashTable[hash]).toBe(CellState.dead);
 	});
 });
